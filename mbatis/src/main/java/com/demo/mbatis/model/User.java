163d3b550876426bf2eee73e89330a392c7b40c9
hunhun <1908529734@qq.com>
2017-09-28 21:03:03 +0800

    一些简单的demo

---
+++ mbatis/src/main/java/com/demo/mbatis/model/User.java
@@ -0,0 +1,73 @@
+package com.demo.mbatis.model;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * Created on 2017/9/25
+ **/
+public class User {
+    private Long id;
+    private Long tenantId;
+
+    private String token;
+    private String nickName;
+
+    private String avatar;
+
+    public User(Long id, Long tenantId, String token, String nickName, String avatar) {
+        this.id = id;
+        this.tenantId = tenantId;
+        this.token = token;
+        this.nickName = nickName;
+        this.avatar = avatar;
+    }
+
+    public User() {
+
+    }
+
+    @Override
+    public String toString() {
+        return JSON.toJSONString(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(Long tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
